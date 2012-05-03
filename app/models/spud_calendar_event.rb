2837f41a77314e17cc63319fa1cd3c01e6fb4d92
Greg Woods <greg@westlakedesign.com>
2012-05-03 15:29:54 -0400

    missed an attr_accessible

--- app/models/spud_calendar_event.rb
+++ app/models/spud_calendar_event.rb
@@ -2,7 +2,7 @@ class SpudCalendarEvent < ActiveRecord::Base
   belongs_to :spud_calendar
   validates_presence_of :title, :start_at, :end_at, :spud_calendar_id
   
-  attr_accessible :title, :start_at, :end_at, :spud_calendar_id, :spud_calendar
+  attr_accessible :title, :description, :start_at, :end_at, :all_day, :spud_calendar_id, :spud_calendar
 
   def self.in_month_of(month)
     where(["spud_calendar_events.start_at <= ? AND spud_calendar_events.start_at >= ?", month.end_of_month, month.beginning_of_month])
