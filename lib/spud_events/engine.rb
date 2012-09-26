944ab82a6dbd6c2936766676740c53ff56365294
David Estes <destes@redwindsw.com>
2012-09-26 19:28:12 -0400

    added require methods to engine

--- lib/spud_events/engine.rb
+++ lib/spud_events/engine.rb
@@ -3,13 +3,21 @@ module Spud
 	module Events
 		 class Engine < Rails::Engine
 			engine_name :spud_events
+
+			def self.require_model(model_name)
+        require "#{root}/app/models/#{model_name}"
+      end
+
+      def self.require_controller(controller_name)
+        require "#{root}/app/controllers/#{controller_name}"
+      end
 			initializer :admin do
 				Spud::Core.configure do |config|
 				  config.admin_applications += [{:name => "Events", :thumbnail => "spud/admin/events_thumb.png", :url => "/spud/admin/events",:order => 10}]
 				end
 			end
-			initializer :assets do |config| 
-				Rails.application.config.assets.precompile += [ 
+			initializer :assets do |config|
+				Rails.application.config.assets.precompile += [
           "spud/events",
           "spud/admin/events"
 				]
