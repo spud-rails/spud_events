78839272ff51b3d7b83f010d8b43f3c127cfe700
David Estes <destes@redwindsw.com>
2013-02-13 20:38:26 -0500

    jquery fix and better date time selector

--- app/assets/javascripts/spud/admin/events.js
+++ app/assets/javascripts/spud/admin/events.js
@@ -6,13 +6,13 @@ Spud.Admin = Spud.Admin || {};
 
 Spud.Admin.Events = new function(){
   var self = this;
-  
+
   self.init = function(){
     // Wd.Admin.tinyMce();
     self.configureColorPicker();
     self.attachCalendarDeleteFX();
   };
-  
+
   self.configureColorPicker = function(){
     var $colorPickers = $('.spud_events_admin_color_picker'),
         $calendarColor = $('#spud_calendar_color');
@@ -31,18 +31,18 @@ Spud.Admin.Events = new function(){
           $(colpkr).fadeOut(500);
   		    return false;
         },
-        onChange: function (hsb, hex, rgb) { 
+        onChange: function (hsb, hex, rgb) {
           console.log("change")
-          
+
           $colorPickers.css('backgroundColor', '#' + hex);
           $calendarColor.attr('value', hex);
         }
       });
     }
   };
-  
+
   self.attachCalendarDeleteFX = function() {
-    $('.js-spud-calendar-delete-action').live('ajax:success', function() {
+    $(document).on('ajax:succes','.js-spud-calendar-delete-action', function() {
       $(this).closest('.spud_events_admin_calendar_legend_item').fadeOut(500, function(){ $(this).remove() });
     });
   }
