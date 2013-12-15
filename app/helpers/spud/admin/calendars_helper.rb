ae24bed9aef0de1d74d811b4f550dc6b42451a3f
David Estes <davydotcom@gmail.com>
2013-12-15 16:22:10 -0500

    fixing spud calendar events links

--- app/helpers/spud/admin/calendars_helper.rb
+++ app/helpers/spud/admin/calendars_helper.rb
@@ -1,13 +1,13 @@
 module Spud::Admin::CalendarsHelper
-  def link_to_previous_calendar_month(date, url_for_options)
+  def link_to_admin_previous_calendar_month(date, url_for_options)
     date = date.advance(:months => -1)
     calendar = params[:calendar] ? params[:calendar] : nil
-    link_to('&laquo; Previous Month'.html_safe, calendar_path(:month => date.month, :year => date.year, :calendar => calendar), :class => 'spud_events_calendar_link previous_calendar btn').html_safe
+    link_to('&laquo; Previous Month'.html_safe, spud_admin_list_spud_calendar_events_path(:month => date.month, :year => date.year, :calendar => calendar), :class => 'spud_events_calendar_link previous_calendar btn').html_safe
   end
 
-  def link_to_next_calendar_month(date, url_for_options)
+  def link_to_admin_next_calendar_month(date, url_for_options)
     date = date.advance(:months => 1)
     calendar = params[:calendar] ? params[:calendar] : nil
-    link_to('Next Month &raquo;'.html_safe, calendar_path(:month => date.month, :year => date.year, :calendar => calendar), :class => 'spud_events_calendar_link next_calendar btn').html_safe
+    link_to('Next Month &raquo;'.html_safe, spud_admin_list_spud_calendar_events_path(:month => date.month, :year => date.year, :calendar => calendar), :class => 'spud_events_calendar_link next_calendar btn').html_safe
   end
 end
