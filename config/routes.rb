bbaa48c575ae0adebfab9537384de5472f750574
David Estes <davydotcom@gmail.com>
2014-01-06 09:56:34 -0500

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
 # Calendar
-  match 'calendar(/:month(/:year(/:calendar)))' => "calendars#show", :as => "calendar"
-  match 'calendar_event/:id' => "calendar_events#show", :as => "event"
+  get 'calendar(/:month(/:year(/:calendar)))' => "calendars#show", :as => "calendar"
+  get 'calendar_event/:id' => "calendar_events#show", :as => "event"
 	namespace :spud do
 		namespace :admin do
       # Events
       resources :calendar_events, :except => [:index]
       resources :calendars, :except => [:index]
-      match 'events(/:month(/:year(/:calendar)))' => "calendar_events#index", :as => "list_spud_calendar_events"
+      get 'events(/:month(/:year(/:calendar)))' => "calendar_events#index", :as => "list_spud_calendar_events"
 		end
 	end
 
